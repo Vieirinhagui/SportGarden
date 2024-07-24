232fc8220d655883e844f8ddae81273046c36682
Vieira <vieiragui147@gmail.com>
2024-07-24 11:15:23 -0300

    Ajuste Home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -4,7 +4,6 @@ import { ServicosSection } from "../../sections/ServicosSection";
 import { QuemSomosSection } from "../../sections/QuemSomosSection";
 import { PortifolioSection } from "../../sections/PortifolioSection";
 import { Footer } from "../../components/Footer";
-import { Onda } from "./styles";
 
 export const Home = () => {
   return (
